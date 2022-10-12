8ad1f35ffb0b8e829cbc85d2bafcf2fe23fc1447
zhoushaowei3 <zhoushaowei3@jd.com>
2022-10-12 20:04:54 +0800

    跨域

---
+++ src/main/java/com/leon/demo/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.leon.demo.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author AnHui_XiaoYang
+ * @Email 939209948@qq.com
+ * @Date 2021/5/10 17:49
+ * @Description
+ */
+@SpringBootConfiguration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOrigins("*")
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+}
