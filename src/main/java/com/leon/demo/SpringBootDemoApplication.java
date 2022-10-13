a341c26b1a5c1dcbae487724447b2f78b1a1f993
zhoushaowei3 <zhoushaowei3@jd.com>
2022-10-13 12:26:53 +0800

    跨域

--- src/main/java/com/leon/demo/SpringBootDemoApplication.java
+++ src/main/java/com/leon/demo/SpringBootDemoApplication.java
@@ -2,17 +2,12 @@ package com.leon.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-
 @MapperScan("com.leon.demo.dao")
-//@ServletComponentScan
-@SpringBootConfiguration
-@EnableWebMvc
+@ServletComponentScan
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
